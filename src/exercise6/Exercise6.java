b113466e404ac78f31c6f31d947cea4c71f6aa18
israel.lopez <israel.lopez@106-13.info2>
2025-09-19 13:12:16 +0200

    Proyectar la dirección y el telefono separados con una línea en blanco

--- src/exercise6/Exercise6.java
+++ src/exercise6/Exercise6.java
@@ -4,8 +4,9 @@ public class Exercise6 {
 
 	public static void main(String[] args) {
 		System.out.println("Israel");
-		System.out.print("Los Molares");
-		System.out.println(" 5457812346");
+		System.out.println("Los Molares");
+		System.out.println();
+		System.out.println("5457812346");
 	}
 
 }
