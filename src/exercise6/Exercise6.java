70e299d9ad7d8e7933173e3016c9ff420b42bb43
israel.lopez <israel.lopez@106-13.info2>
2025-09-19 12:57:33 +0200

    Proyectamos el nombre, dirección y teléfono, pero añadiendo la dirección y telefono en la mmisma línea.

---
+++ src/exercise6/Exercise6.java
@@ -0,0 +1,11 @@
+package exercise6;
+
+public class Exercise6 {
+
+	public static void main(String[] args) {
+		System.out.println("Israel");
+		System.out.print("Los Molares");
+		System.out.println(" 5457812346");
+	}
+
+}
